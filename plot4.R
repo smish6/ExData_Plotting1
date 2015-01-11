55dca012826bf08697ba84b2117a17e82a505d53
smish6 <shashinmishra+smish6@gmail.com>
2015-01-11 22:12:40 +0000

    all work done, final comments remaining

--- plot4.R
+++ plot4.R
@@ -13,9 +13,21 @@ plot4 <- function() {
         # Open the png device
         png("plot4.png")
         
-        # Create the plot as required
+        # Create the plots as required
         par(mfcol = c(2,2))
-        hist(as.numeric(plotData$Global_active_power), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+        
+        plot(x = plotData$TimeStamp, y = plotData$Global_active_power, ylab = "Global Active Power", xlab = "", type = "l")
+        
+        plot(x = plotData$TimeStamp, y = plotData$Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "l")
+        lines(plotData$TimeStamp, plotData$Sub_metering_2, col = "red")
+        lines(plotData$TimeStamp, plotData$Sub_metering_3, col = "blue")
+        legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), cex = 0.5)
+        
+        plot(x = plotData$TimeStamp, y = plotData$Voltage, xlab = "datetime", ylab = "Voltage", type = "l")
+        
+        plot(x = plotData$TimeStamp, y = plotData$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power", type = "l", ylim = c(0.0, 0.5), yaxt = "n")
+        axis(2, at = seq(0.0, 0.5, by = 0.1), las = 1)
+        
         
         # Close the graphics device.
         dev.off()
